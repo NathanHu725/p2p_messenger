6ee0961c7d078923b3845f175b86b2075efdfa8d
Nathan Hu <nahu725@gmail.com>
2023-04-27 12:35:16 -0400

    server working

--- messaging_server/src/handlers.rs
+++ messaging_server/src/handlers.rs
@@ -1,87 +1,83 @@
-use std::net::Shutdown;
 use mio::net::TcpStream;
-use std::io::{Write, Read};
+use mio::Token;
+use std::io::Write;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use std::boxed::Box;
 
-pub type CacheMap = Arc<Mutex<HashMap<String, Vec<String>>>>;
-pub type ConnMap = Arc<Mutex<HashMap<String, TcpStream>>>;
-
-const MDIR: &str = "./messages/";
+pub type CacheMap = HashMap<String, Vec<String>>;
+pub type ConnMap = HashMap<String, Token>;
+pub type SockMap = HashMap<Token, TcpStream>;
 
 // Inspired by rust handbook
-pub fn handle_connection(username: &str, mut stream: &TcpStream, connections: ConnMap, cache: CacheMap) {
-    // Read the message into a buffer
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+// pub fn handle_connection(username: &str, mut stream: &TcpStream, connections: &ConnMap, cache: &CacheMap) {
+//     // Read the message into a buffer
+//     let mut buffer = [0; 1024];
+//     stream.read(&mut buffer).unwrap();
 
-    // Split the message into a status line and a body
-    let as_string = std::str::from_utf8(&buffer).unwrap();
-    let (code, message) = match as_string.split_once(" ") {
-        Some((a, b)) => (a, b),
-        None => {
-            stream.shutdown(Shutdown::Both);
-            connections.lock().unwrap().remove(username);
-            ("ERROR", "")
-        },
-    };
+//     // Split the message into a status line and a body
+//     let as_string = std::str::from_utf8(&buffer).unwrap();
+//     let (code, message) = match as_string.split_once(" ") {
+//         Some((a, b)) => (a, b),
+//         None => {
+//             stream.shutdown(Shutdown::Both);
+//             connections.lock().unwrap().remove(username);
+//             ("ERROR", "")
+//         },
+//     };
 
-    // Handle based on the status code
-    match code {
-        "ACK" => handle_ack(message, cache),
-        "SEND" => handle_send(stream, message, connections, cache),
-        "INIT" => handle_init(stream, message, connections, cache),
-        "IP_RETRIEVAL" => handle_ip_retrieval(stream, message, connections),
-        _ => handle_error(message),
-    };
-}
+//     // Handle based on the status code
+//     match code {
+//         "ACK" => handle_ack(message, cache),
+//         "SEND" => handle_send(stream, message, connections, cache),
+//         "INIT" => handle_init(stream, message, connections, cache),
+//         "IP_RETRIEVAL" => handle_ip_retrieval(stream, message, connections),
+//         _ => handle_error(message),
+//     };
+// }
 
-pub fn handle_init(mut stream: TcpStream, 
+pub fn handle_init(token: &Token, sockets: &mut SockMap, 
                 message: &str, 
-                connections: ConnMap, 
-                cache: CacheMap) {
+                connections: &mut ConnMap, 
+                cache: &CacheMap) {
     println!("received init {}", message);
     // Split the message into tokens
-    let (u, i) = message.split_once(";").unwrap();
-    let (username, _ip) = (String::from(u), String::from(i));
+    let (username, _ip) = message.split_once(";").unwrap();
 
-    let default = Vec::<String>::new();
-    let cache_access = cache.lock().unwrap();
-    let to_write = match cache_access.get(&username) {
+    let mut default = Vec::<String>::new();
+    let to_write = match cache.get(username) {
         Some(v) => v,
-        None => &default,
+        None => &mut default,
     };
 
     let message = format!("UPDATE {}", to_write.join("\n"));
-    drop(cache_access);
+    let stream = sockets.get_mut(&token).unwrap();
 
     stream.write_all(message.as_bytes()).unwrap();
     stream.flush().unwrap();
-    connections.lock().unwrap().insert(username.clone(), stream);
+    connections.insert(username.to_string(), *token);
 }
 
-pub fn handle_send(mut stream: TcpStream, 
+pub fn handle_send(token: &Token, sockets: &mut SockMap, 
     message: &str, 
-    connections: ConnMap, 
-    cache: CacheMap) {
+    connections: &mut ConnMap, 
+    cache: &mut CacheMap) {
     println!("received send {}", message);
     // Pull the sender out
-    let (s, o) = message.split_once(";").unwrap();
-    let (sender, orig_message) = (String::from(s), String::from(o));
+    let (sender, orig_message) = message.split_once(";").unwrap();
+    let orig_message = orig_message.to_string();
 
-    if let Some(conn) = connections.lock().unwrap().get_mut(&sender) {
-        conn.write_all(message.as_bytes()).unwrap();
-        conn.flush().unwrap();
-        if let Some(v) = cache.lock().unwrap().get_mut(&sender) {
-            v.push(orig_message.to_owned());
-            drop(v)
+    if let Some(t) = connections.get(sender) {
+        let stream = sockets.get_mut(&t).unwrap();
+        stream.write_all(message.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        if let Some(v) = cache.get_mut(sender) {
+            v.push(orig_message);
         } else {
-            cache.lock().unwrap().insert(sender, vec![orig_message.to_owned()]);
+            cache.insert(sender.to_string(), vec![orig_message]);
         }
         println!("Sent {}", message);
     } else {
         let message = format!("404 {} not found", sender);
+        let stream = sockets.get_mut(&token).unwrap();
         stream.write_all(message.as_bytes()).unwrap();
         println!("Sent {}", message);
         stream.flush().unwrap();
@@ -89,28 +85,31 @@ pub fn handle_send(mut stream: TcpStream,
 }
 
 pub fn handle_ack(message: &str, 
-    cache: CacheMap) {
+    cache: &mut CacheMap) {
         println!("received ack {}", message);
     // Remove the message from the cache one there is a receipt
-    let (u, o) = message.split_once(";").unwrap();
-    let (username, orig_message) = (String::from(u), String::from(o));
+    let (username, orig_message) = message.split_once(";").unwrap();
 
-    let mut binding = cache.lock().unwrap();
-    let user_cache = binding.get_mut(&username).unwrap();
+    let mut default = Vec::<String>::new();
+    let user_cache: &mut Vec<String> = match cache.get_mut(username) {
+        Some(v) => v,
+        None => &mut default,
+    };
     let index = user_cache.iter().position(|x| *x == orig_message).unwrap();
     user_cache.remove(index);
 }
 
-pub fn handle_ip_retrieval(mut stream: TcpStream, username: &str, connections: ConnMap) {
-    let message = match connections.lock().unwrap().get(username) {
-        Some(conn) => String::from("IP_RETRIEVAL ") + &conn.peer_addr().unwrap().to_string(),
+pub fn handle_ip_retrieval(token: &Token, sockets: &mut SockMap, username: &str, connections: &ConnMap) {
+    let message = match connections.get(username) {
+        Some(t) => String::from("IP_RETRIEVAL ") + &sockets.get_mut(&t).unwrap().peer_addr().unwrap().to_string(),
         None => String::from("404 not found"),
     };
 
+    let stream = sockets.get_mut(&token).unwrap();
     stream.write_all(message.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
 pub fn handle_error(message: &str) {
-    
+    println!("received error {}", message);
 }
--- messaging_server/src/main.rs
+++ messaging_server/src/main.rs
@@ -1,27 +1,81 @@
-use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
 use local_ip_address::local_ip;
 use mio::net::TcpListener;
 use mio::{Events, Poll, Token, Interest};
 use std::io::{self, Read};
-use std::thread;
-use handlers::{CacheMap, ConnMap, handle_ack, handle_connection, handle_error, handle_init, handle_ip_retrieval};
+use handlers::{CacheMap, ConnMap, SockMap, handle_send, handle_ack, handle_error, handle_init, handle_ip_retrieval};
 
 const PORT: u16 = 8013;
 const LISTENER: Token = Token(0);
 
-fn setup_server(conn: ConnMap, cache: CacheMap) {
+fn listener_poll(listener: &mut TcpListener, poll: &Poll, sockets: &mut SockMap, socket_index: &mut usize) {
+    loop {
+        match listener.accept() {
+            Ok((mut socket, _)) => {
+                // Get the token for the socket
+                let token = Token(*socket_index);
+                *socket_index += 1;
+
+                // Register the new socket w/ poll
+                poll.registry().register(&mut socket, token, 
+                    Interest::READABLE | Interest::WRITABLE).unwrap();
+
+                // Store the socket
+                sockets.insert(token, socket);
+            }
+            // Socket is not ready anymore, stop accepting
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                break;
+            }
+            // Unexpected error
+            e => panic!("err={:?}", e), 
+        }
+    }
+}
+
+fn token_poll(token: &Token, sockets: &mut SockMap, buf: &mut [u8], connections: &mut ConnMap, cache: &mut CacheMap) {
+    loop {
+        match sockets.get_mut(&token).unwrap().read(buf) {
+            Ok(0) => {
+                // Socket is closed, remove it from the map
+                sockets.remove(&token);
+                break;
+            }
+            // Data is not actually sent in this example
+            Ok(_) => {
+                let (code, message) = std::str::from_utf8(buf).unwrap().split_once(" ").unwrap();
+                
+                // Handle based on the status code
+                match code {
+                    "ACK" => handle_ack(message, cache),
+                    "SEND" => handle_send(token, sockets, message, connections, cache),
+                    "INIT" => handle_init(token, sockets, message, connections, cache),
+                    "IP_RETRIEVAL" => handle_ip_retrieval(token, sockets, message, connections),
+                    _ => handle_error(message),
+                };
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                // Socket is not ready anymore, stop reading
+                break;
+            }
+            e => panic!("err={:?}", e), // Unexpected error
+        }
+    }
+}
+
+fn run_server(mut conn: ConnMap, mut cache: CacheMap) {
     // Create poll and appropriate objects
     let mut poll = Poll::new().unwrap();
-    let mut sockets = HashMap::new();
+    let mut sockets: SockMap = HashMap::new();
     let mut events = Events::with_capacity(1024);
     let mut socket_index = 1;
 
     // Create listener and buffer
-    let listener = TcpListener::bind(
+    let mut listener = TcpListener::bind(
         format!("{:?}:{}", local_ip().unwrap(), 
             PORT).parse().unwrap()
     ).unwrap();
+    poll.registry().register(&mut listener, LISTENER, Interest::READABLE).unwrap();
     let mut buf = [0; 1024];
 
     loop {
@@ -32,57 +86,10 @@ fn setup_server(conn: ConnMap, cache: CacheMap) {
         for event in &events {
             match event.token() {
                 LISTENER => {
-                    loop {
-                        match listener.accept() {
-                            Ok((mut socket, _)) => {
-                                // Get the token for the socket
-                                let token = Token(socket_index);
-                                socket_index += 1;
-    
-                                // Register the new socket w/ poll
-                                poll.registry().register(&mut socket, token, 
-                                    Interest::READABLE | Interest::WRITABLE).unwrap();
-    
-                                // Store the socket
-                                sockets.insert(token, socket);
-                            }
-                            // Socket is not ready anymore, stop accepting
-                            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                                break;
-                            }
-                            // Unexpected error
-                            e => panic!("err={:?}", e), 
-                        }
-                    }
+                    listener_poll(&mut listener, &poll, &mut sockets, &mut socket_index);
                 }
                 token => {
-                    loop {
-                        match sockets.get_mut(&token).unwrap().read(&mut buf) {
-                            Ok(0) => {
-                                // Socket is closed, remove it from the map
-                                sockets.remove(&token);
-                                break;
-                            }
-                            // Data is not actually sent in this example
-                            Ok(message) => {
-                                let (code, message) = match as_string.split_once(" ").unwrap();
-                                
-                                // Handle based on the status code
-                                match code {
-                                    "ACK" => handle_ack(message, cache),
-                                    "SEND" => handle_send(stream, message, connections, cache),
-                                    "INIT" => handle_init(stream, message, connections, cache),
-                                    "IP_RETRIEVAL" => handle_ip_retrieval(stream, message, connections),
-                                    _ => handle_error(message),
-                                };
-                            },
-                            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                                // Socket is not ready anymore, stop reading
-                                break;
-                            }
-                            e => panic!("err={:?}", e), // Unexpected error
-                        }
-                    }
+                    token_poll(&token, &mut sockets, &mut buf, &mut conn, &mut cache);
                 }
             }
         }
@@ -90,9 +97,9 @@ fn setup_server(conn: ConnMap, cache: CacheMap) {
 }
 
 fn main() {
-    let active_connections = Arc::new(Mutex::new(HashMap::new()));
-    let cached_messages = Arc::new(Mutex::new(HashMap::new()));
-    setup_server(active_connections.clone(), cached_messages.clone());
+    let active_connections: ConnMap = HashMap::new();
+    let cached_messages: CacheMap = HashMap::new();
+    run_server(active_connections, cached_messages);
 
     println!("Hello, world!");
 }
