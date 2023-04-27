96cb4d473763d9e1f97a2f66fda6ada291f6bd5b
Nathan Hu <nahu725@gmail.com>
2023-04-26 23:51:37 -0400

    updates

--- messaging_server/src/handlers.rs
+++ messaging_server/src/handlers.rs
@@ -1,4 +1,4 @@
-use std::net::TcpStream;
+use std::net::{TcpStream, Shutdown};
 use std::io::{Write, Read};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
@@ -7,14 +7,21 @@ use std::boxed::Box;
 const MDIR: &str = "./messages/";
 
 // Inspired by rust handbook
-pub fn handle_connection(mut stream: TcpStream, connections: Arc<Mutex<HashMap<String, TcpStream>>>, cache: Arc<Mutex<HashMap<String, Vec<String>>>>) {
+pub fn handle_connection(username: &str, mut stream: TcpStream, connections: Arc<Mutex<HashMap<String, TcpStream>>>, cache: Arc<Mutex<HashMap<String, Vec<String>>>>) {
     // Read the message into a buffer
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     // Split the message into a status line and a body
     let as_string = std::str::from_utf8(&buffer).unwrap();
-    let (code, message) = as_string.split_once(" ").unwrap();
+    let (code, message) = match as_string.split_once(" ") {
+        Some((a, b)) => (a, b),
+        None => {
+            stream.shutdown(Shutdown::Both);
+            connections.lock().unwrap().remove(username);
+            ("ERROR", "")
+        },
+    };
 
     // Handle based on the status code
     match code {
@@ -30,6 +37,7 @@ fn handle_init(mut stream: TcpStream,
                 message: &str, 
                 connections: Arc<Mutex<HashMap<String, TcpStream>>>, 
                 cache: Arc<Mutex<HashMap<String, Vec<String>>>>) {
+    println!("received init {}", message);
     // Split the message into tokens
     let (u, i) = message.split_once(";").unwrap();
     let (username, _ip) = (String::from(u), String::from(i));
@@ -42,6 +50,7 @@ fn handle_init(mut stream: TcpStream,
     };
 
     let message = format!("UPDATE {}", to_write.join("\n"));
+    drop(cache_access);
 
     stream.write_all(message.as_bytes()).unwrap();
     stream.flush().unwrap();
@@ -52,6 +61,7 @@ fn handle_send(mut stream: TcpStream,
     message: &str, 
     connections: Arc<Mutex<HashMap<String, TcpStream>>>, 
     cache: Arc<Mutex<HashMap<String, Vec<String>>>>) {
+    println!("received send {}", message);
     // Pull the sender out
     let (s, o) = message.split_once(";").unwrap();
     let (sender, orig_message) = (String::from(s), String::from(o));
@@ -61,18 +71,22 @@ fn handle_send(mut stream: TcpStream,
         conn.flush().unwrap();
         if let Some(v) = cache.lock().unwrap().get_mut(&sender) {
             v.push(orig_message.to_owned());
+            drop(v)
         } else {
             cache.lock().unwrap().insert(sender, vec![orig_message.to_owned()]);
         }
+        println!("Sent {}", message);
     } else {
         let message = format!("404 {} not found", sender);
         stream.write_all(message.as_bytes()).unwrap();
+        println!("Sent {}", message);
         stream.flush().unwrap();
     }
 }
 
 fn handle_ack(message: &str, 
     cache: Arc<Mutex<HashMap<String, Vec<String>>>>) {
+        println!("received ack {}", message);
     // Remove the message from the cache one there is a receipt
     let (u, o) = message.split_once(";").unwrap();
     let (username, orig_message) = (String::from(u), String::from(o));
@@ -94,5 +108,5 @@ fn handle_ip_retrieval(mut stream: TcpStream, username: &str, connections: Arc<M
 }
 
 fn handle_error(message: &str) {
-    println!("Error: {}", message);
+    
 }
--- messaging_server/src/main.rs
+++ messaging_server/src/main.rs
@@ -5,37 +5,63 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use threadpool::ThreadPool;
 
+mod handlers;
 use handlers::handle_connection;
 
 const PORT: u16 = 8013;
 
 fn setup_server(conn: Arc<Mutex<HashMap<String, TcpStream>>>, 
                 cache: Arc<Mutex<HashMap<String, Vec<String>>>>) {
-    // thread::spawn(move || {
-        let listener = TcpListener::bind(
-            format!("{}:{}", local_ip().unwrap(), PORT)
-        ).unwrap();
-    
+    {
+        let conn = conn.clone();
+        let cache = cache.clone();
+        
+        thread::spawn(move || {
+            let listener = TcpListener::bind(
+                format!("{:?}:{}", local_ip().unwrap(), PORT)
+            ).unwrap();
+        
+            // Set up the thread pool
+            let num_workers = 4;
+            let pool = ThreadPool::new(num_workers);
+        
+            // Each message that comes in is passed to the thread pool
+            for stream in listener.incoming() {
+                let stream = stream.unwrap();
+                let temp_conn = conn.clone();
+                let temp_cache = cache.clone();
+                pool.execute(move || {
+                    handle_connection("", stream, temp_conn, temp_cache);
+                });
+            }
+        });
+    }
+
+    loop {
         // Set up the thread pool
         let num_workers = 4;
         let pool = ThreadPool::new(num_workers);
-    
-        // Each message that comes in is passed to the thread pool
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
-            let temp_conn = conn.clone();
-            let temp_cache = cache.clone();
-            pool.execute(move || {
-                handle_connection(stream, temp_conn, temp_cache);
-            });
-        }
-    // });
+
+        for (username, stream) in conn.lock().unwrap().iter() {
+            let stream = stream.try_clone().expect("failed");
+            let mut buf = [0; 10];
+            if let Ok(something) = stream.peek(&mut buf) {
+                let username = username.clone();
+                println!("Found 1");
+                let temp_conn = conn.clone();
+                let temp_cache = cache.clone();
+                pool.execute(move || {
+                    handle_connection(&username, stream, temp_conn, temp_cache);
+                });
+            }
+        };
+    }
 }
 
 
 fn main() {
-    let mut active_connections = Arc::new(Mutex::new(HashMap::new()));
-    let mut cached_messages = Arc::new(Mutex::new(HashMap::new()));
+    let active_connections = Arc::new(Mutex::new(HashMap::new()));
+    let cached_messages = Arc::new(Mutex::new(HashMap::new()));
     setup_server(active_connections.clone(), cached_messages.clone());
 
     println!("Hello, world!");
